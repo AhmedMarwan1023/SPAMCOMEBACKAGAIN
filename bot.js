fd382f49a566a97f4f5081bcef62172a3251205a
AhmedMarwan1023 <44772647+AhmedMarwan1023@users.noreply.github.com>
2019-01-05 11:43:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("526372495985737728")
+let channel =     client.channels.get("531060188359426049")
 setInterval(function() {
 channel.send(`** Spam سبام ديم مان اح هههههههه سبام نيهاهااهاها سبام يبويييييي ست داون بوي Spam**`);
 }, 30)
